81d02a534c4c50314bd820ad50420731e08171ee
Juan Quiroz <juan.quiroz1009@gmail.com>
2024-01-25 21:42:36 -0500

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<!--scripts-->
+<script src="http://localhost/php_mysql_crud/assets/js/bootstrap.min.js"></script>
+<!--Fontawesome-->
+<script src="https://kit.fontawesome.com/aae16d7d7d.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP MYSQL CRUD</title>
+  <!--Bootstrap 5-->
+  <link href="http://localhost/php_mysql_crud/assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+  </nav>
